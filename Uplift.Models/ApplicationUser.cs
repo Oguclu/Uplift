76a10bbf58159ff4246ffae67f84e2a7dca1ae53
FO EESE CV&S <39525560+eesecvs@users.noreply.github.com>
2020-06-07 12:32:39 +0300

    initial commit

---
+++ Uplift.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public String Name { get; set; }
+        public String StreetAddress { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String PostalCode { get; set; }
+    }
+}
---
+++ Uplift/Extensions/SessionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void setObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T getObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            if (value == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(value);
+            }
+        }
+    }
+}
